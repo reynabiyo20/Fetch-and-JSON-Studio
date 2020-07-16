4184fe75b9af136f9d58359ade6abe53ad0a20e1
reyna biyo <reynabiyo20@gmail.com>
2020-07-16 13:33:14 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// TODO: add code here
+window.addEventListener("load", function () {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+        response.json().then(function (json) {
+            const astronauts = document.getElementById("container");
+            for (astronaut of json) {
+                astronauts.innerHTML += `<fieldset class="astronaut">
+                <div class="bio">
+                    <h3> ${astronaut.firstName} ${astronaut.lastName}</h3>
+                    <ul>
+                         <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                        <li>Active: ${astronaut.active}</li>
+                        <li>Skills: ${astronaut.skills}</li>
+                    </ul>
+                </div>
+                <img class="avatar" src=${astronaut.picture}>
+                </fieldset>`;
+
+                 }
+        });
+    });
+});
